5e2c8daa7ab150ce4afda53a89c6edc2df6b1f76
blanche82 <adelajda.kolodziejska@gmail.com>
2019-02-27 22:11:57 +0100

    comments added

--- code/script.js
+++ code/script.js
@@ -5,64 +5,65 @@ const main = document.querySelector("main");
 //make shortcuts to API endpoints
 const link = "https://spreadsheets.google.com/feeds/list/1RVK5ksJDyymK5QzZwrJ2KYxq5KKuAsBbbH0gmk9czps/1/public/values?alt=json";
 
-var database = null;
+var database;
 
 //define functions
 function loadJSON(link){
-	fetch(link).then(e=>e.json()).then(data=>database = data.feed.entry);
+	fetch(link).then(e=>e.json()).then(data=>database = data.feed.entry); // load JSON and save it in the 'database' variable
 }
 
 // add a lego set to displayed sets
 function displayLegoData(legoSet) {
-	let newSet = template.cloneNode(true);
+	let newSet = template.cloneNode(true); // clone the template and set all the fields
 	newSet.querySelector('.setTitleFront').textContent = legoSet.gsx$title.$t;
 	newSet.querySelector('.setTitleBack').textContent = legoSet.gsx$title.$t;
 	newSet.querySelector('.setPicture').src = "photos/" + legoSet.gsx$imagename.$t + ".jpg";
 	newSet.querySelector("p").textContent = legoSet.gsx$description.$t;
-	newSet.querySelectorAll(".control").forEach(control => control.classList.add("bgColorLinear" + legoSet.gsx$category.$t));
+	newSet.querySelectorAll(".control").forEach(control => control.classList.add("bgColorLinear" + legoSet.gsx$category.$t)); // set appropriate flipping buttons color
 	main.appendChild(newSet);
 }
 
 //function for showing only the clicked category
 function changeCategory(cat) { 
-	document.querySelectorAll(".topMenuPosition").forEach(menuPosition => {
+	document.querySelectorAll(".topMenuPosition").forEach(menuPosition => { // find the top menu position of the selected class and mark it as 'selected'
 		if (menuPosition.dataset.category == cat) menuPosition.classList.add("topMenuSelected") 
 		else menuPosition.classList.remove("topMenuSelected")
 	});
-	document.querySelectorAll(".mainMenuSection").forEach(menuSection => {
+	document.querySelectorAll(".mainMenuSection").forEach(menuSection => { // hide all the tiles in the tile menu but the one we selected
 		if (menuSection.dataset.category == cat) menuSection.classList.remove("hide") 
 		else menuSection.classList.add("hide")
 	});
-	while (main.firstChild) main.firstChild.remove();
-    database.forEach(legoSet => { if (legoSet.gsx$category.$t == cat) displayLegoData(legoSet) });
+	while (main.firstChild) main.firstChild.remove(); // remove all content from any category that was selected previously
+    database.forEach(legoSet => { if (legoSet.gsx$category.$t == cat) displayLegoData(legoSet) }); // display entries from the database that match the selected category 
 }
 
 function flip(article) {
-	let currentFront = article.querySelector(".over");
-	let currentBack = article.querySelector(".under");
-	currentFront.addEventListener("animationend", function onHalfTurn(event) {
-		currentFront.removeEventListener("animationend", onHalfTurn);
-		currentFront.classList.remove("over");
-		currentFront.classList.add("under");
-		currentFront.classList.remove("turnDissapearC");
-		currentBack.addEventListener("animationend", function onFullTurn(event) { 
-			currentBack.removeEventListener("animationend", onFullTurn);
-			currentBack.classList.remove("turnAppearC");
+	let currentFront = article.querySelector(".over"); // the section that is currently on top
+	let currentBack = article.querySelector(".under"); // the section that is currently hidden
+	currentFront.addEventListener("animationend", function onHalfTurn(event) { // this event listener will activate after 'currentFront' is rotated 90 degrees
+		currentFront.removeEventListener("animationend", onHalfTurn); // remove the function from the currentFront
+		currentFront.classList.remove("over"); // it is no longer 'over' 
+		currentFront.classList.add("under"); // now it is hidden
+		currentFront.classList.remove("turnDissapearC"); // remove animation class
+		currentBack.addEventListener("animationend", function onFullTurn(event) {  // add event listener which will activate upon complete flip, to do cleanup
+			currentBack.removeEventListener("animationend", onFullTurn);  // remove event handler
+			currentBack.classList.remove("turnAppearC"); // remove animation class
 		});
-		currentBack.classList.add("over");
-		currentBack.classList.add("turnAppearC");
-		currentBack.classList.remove("under");
+		currentBack.classList.add("over"); // 'currentBack' is the new top section 
+		currentBack.classList.add("turnAppearC"); // add animation to turn it from 90 degrees to 0
+		currentBack.classList.remove("under"); // remove 'under' - now it is visible
 	});
-	currentFront.classList.add("turnDissapearC");
+	currentFront.classList.add("turnDissapearC"); // start the flip animation 
 }
 
 function home() {
-	document.querySelectorAll(".mainMenuSection").forEach(menuSection => menuSection.classList.remove("hide"));
-	document.querySelectorAll(".topMenuPosition").forEach(menuPosition => {
+	document.querySelectorAll(".mainMenuSection").forEach(menuSection => menuSection.classList.remove("hide")); // show all the tiles in the tile menu
+	document.querySelectorAll(".topMenuPosition").forEach(menuPosition => { // mark 'home' as selected and all others as not selected
 		if (menuPosition.dataset.category == 'Home') menuPosition.classList.add("topMenuSelected") 
 		else menuPosition.classList.remove("topMenuSelected")
 	});
-	while (main.firstChild) main.firstChild.remove();
+	while (main.firstChild) main.firstChild.remove(); // remove all the content entries, as they are not on the home page
 }
 
+document.querySelectorAll(".mainMenuSection").forEach(menuSection => menuSection.classList.remove("hide")); // show all the tiles in the tile menu
 loadJSON(link);
